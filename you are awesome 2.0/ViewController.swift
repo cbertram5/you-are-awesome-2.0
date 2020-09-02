a6265584932da6a80e51d650d4a5cec6c5a06efe
Chris Bertram <bertramc@bc.edu>
2020-09-01 22:36:14 -0400

    Initial Commit

---
+++ you are awesome 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  you are awesome 2.0
+//
+//  Created by Chris Bertram on 9/1/20.
+//  Copyright © 2020 Chris Bertram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
